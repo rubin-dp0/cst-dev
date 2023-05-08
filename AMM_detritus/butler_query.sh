f7045c79d49f30fef83e396c63cedc07d043303a
Aaron Meisner <aaron.meisner@noirlab.edu>
2023-05-08 09:26:47 +0000

    command line butler query

---
+++ AMM_detritus/butler_query.sh
@@ -0,0 +1,2 @@
+# command line butler query demonstrating that the detection step wrote out deepCoadd_calexp products
+butler query-datasets --collections u/ameisner/nb9b_spe dp02 deepCoadd_calexp |grep nb9b_spe
