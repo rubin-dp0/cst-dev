1ec25f9c13f94d5c360db63603faccba2846bcd9
Aaron Meisner <aaron.meisner@noirlab.edu>
2023-08-26 09:06:15 +0000

    solution for final optional exercise in command line custom coadds tutorial

---
+++ AMM_detritus/qgraph_no_show_graph.sh
@@ -0,0 +1,7 @@
+pipetask qgraph \
+-b dp02 \
+-i 2.2i/runs/DP0.2 \
+-p $DRP_PIPE_DIR/pipelines/LSSTCam-imSim/DRP-test-med-1.yaml#makeWarp,assembleCoadd \
+-c makeWarp:doApplyFinalizedPsf=False \
+-c makeWarp:connections.visitSummary="visitSummary" \
+-d "tract = 4431 AND patch = 17 AND visit in (919515,924057,924085,924086,929477,930353) AND skymap = 'DC2'"
