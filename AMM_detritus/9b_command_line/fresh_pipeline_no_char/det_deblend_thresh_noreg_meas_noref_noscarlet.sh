7c2503bda04fee92615d07be992e7afb82ae33fc
Aaron Meisner <aaron.meisner@noirlab.edu>
2023-10-09 09:37:21 +0000

    some more progress but still failing

---
+++ AMM_detritus/9b_command_line/fresh_pipeline_no_char/det_deblend_thresh_noreg_meas_noref_noscarlet.sh
@@ -0,0 +1,21 @@
+# tried adding the following:
+#    -c measure:connections.refCat=None \
+# but this failed validation (i think because None is not an acceptable value...)
+# "lsst.pex.config.config.FieldValidationError: Field 'connections.refCat' failed validation: Required value cannot be None"
+
+LOGFILE=$LOGDIR/det_deblend_thresh_noreg_meas_noref_noscarlet.log; \
+date | tee $LOGFILE; \
+pipetask --long-log --log-file $LOGFILE run \
+-b dp02 \
+-i u/$USER/custom_coadd_window1_test1,2.2i/runs/DP0.2 \
+-o u/$USER/custom_coadd_window1_test1_O \
+-c detection:detection.thresholdValue=10 \
+-c detection:detection.thresholdType="stdev" \
+-c measure:doMatchSources=False \
+-c measure:doWriteMatchesDenormalized=False \
+-c measure:doPropagateFlags=False \
+-c measure:inputCatalog="deblendedFlux" \
+-c measure:connections.refCat="cal_ref_cat_2_2" \
+-p nb9b_det_deblend_meas.yaml#detection,deblend_single,measure \
+-d "tract = 4431 AND patch = 17 AND band = 'i' AND skymap = 'DC2'"; \
+date | tee -a $LOGFILE
